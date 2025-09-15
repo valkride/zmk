af60d3cd7b51e5b302c66753518627924c6d5814
Valkride <venzovossen@outlook.com>
2025-09-15 13:12:06 +0200

    modified:   app/src/Dictionary/dict_a.h 	new file:   app/src/Dictionary/dict_apostrophe.h 	modified:   app/src/Dictionary/dict_b.h 	modified:   app/src/Dictionary/dict_c.h 	modified:   app/src/Dictionary/dict_d.h 	modified:   app/src/Dictionary/dict_e.h 	modified:   app/src/Dictionary/dict_f.h 	modified:   app/src/Dictionary/dict_g.h 	modified:   app/src/Dictionary/dict_h.h 	modified:   app/src/Dictionary/dict_i.h 	modified:   app/src/Dictionary/dict_j.h 	modified:   app/src/Dictionary/dict_k.h 	modified:   app/src/Dictionary/dict_l.h 	modified:   app/src/Dictionary/dict_m.h 	modified:   app/src/Dictionary/dict_n.h 	modified:   app/src/Dictionary/dict_o.h 	modified:   app/src/Dictionary/dict_p.h 	modified:   app/src/Dictionary/dict_q.h 	modified:   app/src/Dictionary/dict_r.h 	modified:   app/src/Dictionary/dict_s.h 	modified:   app/src/Dictionary/dict_t.h 	modified:   app/src/Dictionary/dict_u.h 	modified:   app/src/Dictionary/dict_v.h 	modified:   app/src/Dictionary/dict_w.h 	modified:   app/src/Dictionary/dict_x.h 	modified:   app/src/Dictionary/dict_y.h 	modified:   app/src/Dictionary/dict_z.h 	deleted:    app/src/Dictionary/dictionary_data.h 	deleted:    app/src/Dictionary/dictionary_lookup.h 	deleted:    app/src/Dictionary/dictionary_simple.h 	deleted:    app/src/Dictionary/spell_checker_dict.h 	deleted:    app/src/Dictionary/test_dictionary.c

--- app/src/Dictionary/dict_x.h
+++ app/src/Dictionary/dict_x.h
@@ -2,7 +2,24 @@
 #define DICT_X_H
 
 static const char* const dict_x[] = {
-    "x"
+    "xa", "xalostockite", "xanadu", "xantha", "xanthaline", "xanthamic", "xanthamid", "xanthamide",
+    "xanthan", "xanthane", "xanthans", "xanthate", "xanthates", "xanthation", "xanthe", "xanthein",
+    "xantheins", "xanthelasma", "xanthelasmic", "xanthelasmoidea", "xanthene", "xanthenes", "xanthian", "xanthic",
+    "xanthid", "xanthide", "xanthidium", "xanthin", "xanthindaba", "xanthine", "xanthines", "xanthins",
+    "xanthinthique", "xanthinuria", "xanthione", "xanthippe", "xanthism", "xanthisma", "xanthite", "xanthium",
+    "xanthiuria", "xanthocarpous", "xanthocephalus", "xanthoceras", "xanthochroi", "xanthochroia", "xanthochroic", "xanthochroid",
+    "xanthochroism", "xanthochromia", "xanthochromic", "xanthochroous", "xanthocobaltic", "xanthocone", "xanthoconite", "xanthocreatinine",
+    "xanthocyanopia", "xanthocyanopsia", "xanthocyanopsy", "xanthocyanopy", "xanthoderm", "xanthoderma", "xanthodermatous", "xanthodont",
+    "xanthodontous", "xanthogen", "xanthogenamic", "xanthogenamide", "xanthogenate", "xanthogenic", "xantholeucophore", "xanthoma",
+    "xanthomas", "xanthomata", "xanthomatosis", "xanthomatous", "xanthomelanoi", "xanthomelanous", "xanthometer", "xanthomonas",
+    "xanthomyeloma", "xanthone", "xanthones", "xanthophane", "xanthophore", "xanthophose", "xanthophyceae", "xanthophyl",
+    "xanthophyll", "xanthophyllic", "xanthophyllite", "xanthophyllous", "xanthopia", "xanthopicrin", "xanthopicrite", "xanthoproteic",
+    "xanthoprotein", "xanthoproteinic", "xanthopsia", "xanthopsin", "xanthopsydracia", "xanthopterin", "xanthopurpurin", "xanthorhamnin",
+    "xanthorrhiza", "xanthorrhoea", "xanthosiderite", "xanthosis", "xanthosoma", "xanthospermous", "xanthotic", "xanthoura",
+    "xanthous", "xanthoxalis", "xanthoxenite", "xanthoxylin", "xanthrochroid", "xanthuria", "xanthus", "xanthydrol",
+    "xanthyl", "xantippe", "xarque", "xat", "xaverian", "xavier", "xaviera", "xavler",
+    "xb", "xbt", "xc", "xcf", "xcl", "xctl", "xd", "xdiv",
+    "xdmcp"
 };
 
 static const size_t DICT_X_SIZE = sizeof(dict_x) / sizeof(dict_x[0]);
